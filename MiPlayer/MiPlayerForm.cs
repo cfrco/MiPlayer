a887718646e21c5f9dc862fa52688908c536c8ba
Cat Xiao <z82206.cat@gmail.com>
2021-05-26 22:38:17 +0800

    init

---
+++ MiPlayer/MiPlayerForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MiPlayer
+{
+    public partial class MiPlayerForm : Form
+    {
+        [DllImport("winmm.DLL", EntryPoint = "PlaySound", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
+        private static extern bool PlaySound(string szSound, IntPtr hMod, PlaySoundFlags flags);
+
+        [Flags]
+        public enum PlaySoundFlags : int
+        {
+            SND_SYNC = 0x0000,
+            SND_ASYNC = 0x0001,
+            SND_NODEFAULT = 0x0002,
+            SND_LOOP = 0x0008,
+            SND_NOSTOP = 0x0010,
+            SND_NOWAIT = 0x00002000,
+            SND_FILENAME = 0x00020000,
+            SND_RESOURCE = 0x00040004
+        }
+
+        private string playWavPath = "";
+
+        public MiPlayerForm(string path)
+        {
+            playWavPath = path;
+
+            InitializeComponent();
+
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+
+            Shown += MiPlayerForm_Shown;
+        }
+
+        private void MiPlayerForm_Shown(object sender, EventArgs e)
+        {
+            Hide();
+
+            PlaySound(playWavPath, new IntPtr(),
+                (PlaySoundFlags.SND_FILENAME | PlaySoundFlags.SND_SYNC | PlaySoundFlags.SND_NODEFAULT));
+
+            Close();
+        }
+    }
+}
---
+++ MiPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPlayer
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string playWavPath = @"C:\Windows\Media\Windows Ding.wav";
+            if (args != null && args.Length > 0)
+            {
+                playWavPath = args[0];
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MiPlayerForm(playWavPath));
+        }
+    }
+}
